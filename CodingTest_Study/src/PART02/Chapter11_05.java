00048fe6162de28a0b172e34810bcc846504f187
kst5959 <52456639+kst5959@users.noreply.github.com>
2021-05-05 15:02:21 +0900

    볼링공 고르기

--- CodingTest_Study/src/PART02/Chapter11_05.java
+++ CodingTest_Study/src/PART02/Chapter11_05.java
@@ -1,10 +1,36 @@
 package PART02;
 
+import java.util.Scanner;
+
 public class Chapter11_05 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		int[] arr = new int[N];
+		
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		int cnt = 0;
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i; j < arr.length; j++) {
+				if(arr[i] == arr[j])
+				{
+					continue;
+				}
+				else
+				{
+					cnt++;
+				}
+			}
+		}
+		
+		System.out.println(cnt);
+		
 	}
 
 }
