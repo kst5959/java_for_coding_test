182a3afd41c27da9bc2dc94e52bd8fbaa00770d5
kst5959 <52456639+kst5959@users.noreply.github.com>
2021-05-15 16:12:13 +0900

    프로그래머스 2LV 문자열 압축 2020 카카오 블라인드 채용

--- CodingTest_Study/src/PART02/Chapter12_09.java
+++ CodingTest_Study/src/PART02/Chapter12_09.java
@@ -1,10 +1,74 @@
 package PART02;
 
-public class Chapter12_09 {
+import java.util.*;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        int step = 1;
+        int[] slice_Arr = new int[s.length()/2];
+        
+        if(s.length() == 1) return 1;
+        
+        for(int i=0; i<s.length()/2; i++)
+        {
+            int slice_length = bruteForce(s, step);
+            slice_Arr[i] = slice_length;
+            step++;
+        }
+        
+        Arrays.sort(slice_Arr);
+        
+        return slice_Arr[0];
+    }
+    
+    private int bruteForce(String s, int step) {
+        String slice = s.substring(0,step);
+        String compress ="";
+        int compressCount = 1;
+        //System.out.println("Slice : " + slice);
+        
+        for(int i=slice.length(); i<=s.length()-step; i++)
+        {
+            if(s.substring(i,i+step).equals(slice))
+            {
+                compressCount++;
+            }
+            else
+            {
+                if(compressCount <= 1)
+                {
+                    compress += slice;    
+                }
+                else
+                {
+                    compress += (Integer.toString(compressCount) + slice);    
+                }
+                
+                slice = s.substring(i,i+step);
+                //System.out.println(slice);
+                compressCount = 1;
+                
+            }
+            i += (step-1);
+        }
+        
+        if(compressCount <= 1)
+        {
+            compress += slice;    
+        }
+        else
+        {
+            compress += (Integer.toString(compressCount) + slice);    
+        }
+        int remain = s.length() % step;
+        if(remain > 0)
+        {
+            compress += s.substring(s.length()-remain, s.length());
+        }
+        
+       // System.out.println("Step : "+ step +" 압축 결과 : "+compress);
+        return compress.length();
+        
+    }
 }
