51968e07ee06126a0caa32429b409fb46b8527e5
kst5959 <52456639+kst5959@users.noreply.github.com>
2021-05-17 22:33:19 +0900

    고정점 찾기 : target값이 주어지지 않는 경우의 이진 탐색 문제 복습

--- CodingTest_Study/src/PART02/Chapter15_28.java
+++ CodingTest_Study/src/PART02/Chapter15_28.java
@@ -6,48 +6,52 @@ public class Chapter15_28 {
 
 	public static int N;
 	public static int[] arr;
-	public static int FixedPoint = 0;
 	
-	public static int binarySearch(int[] arr, int start,int end)
+	
+	//target 값이 명확히 주어지지 않은 경우 배열의 mid 값(arr[mid])을 target으로 선정하여 이진 탐색을 수행한다
+	public static int binarySearch(int[] arr, int FixedPoint ,int start, int end)
 	{
-		while(start <= end)
+		while(start < end)
 		{
-			//mid가 target이라고 가정하고 이진탐색
 			int mid = (start + end) /2;
 			
-			//찾은 경우 중간값 인덱스 반환
+			//고정점인경우 : 증간값과 그 인덱스 값이 일치하는 경우
 			if(arr[mid] == mid)
 			{
-				return mid;
+				FixedPoint = arr[mid];
+				return FixedPoint;
 			}
-			//중간점의 값보다 찾고자 하는 값이 작은 경우 왼쪽 확인
 			else if(arr[mid] > mid)
 			{
-				end = mid - 1;
+				end = mid;
 			}
-			//중간점의 값보다 찾고자 하는 값이 큰 경우 오른쪽 확인
 			else
 			{
 				start = mid + 1;
 			}
+			
 		}
 		
-		return -1;
+		
+		return FixedPoint;
 	}
-	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
 		arr = new int[N];
+		
+		
 		for (int i = 0; i < N; i++) 
 		{
 			arr[i] = sc.nextInt();
 		}
 		
-		FixedPoint = binarySearch(arr, 0, arr.length);
+		int FixedPoint = binarySearch(arr, -1 ,0, N);
+		
 		System.out.println(FixedPoint);
+		
 	}
 
 	
