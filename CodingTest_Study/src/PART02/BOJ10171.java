668d90f5ee8b40476bf90e2cd62a1325dfbb76be
kst5959 <52456639+kst5959@users.noreply.github.com>
2021-06-10 22:00:27 +0900

    백준 10171 고양이 구현 : 예제에 나온 답을 복사해서 넣었더니 정답이 된다..

---
+++ CodingTest_Study/src/PART02/BOJ10171.java
@@ -0,0 +1,15 @@
+package PART02;
+
+public class BOJ10171 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.printf("\\    /\\" + "\n");
+		System.out.printf(" )  ( ')" + "\n");
+		System.out.printf("(  /  )" + "\n");
+		System.out.printf(" \\(__)|" + "\n");
+	
+	}
+
+}
