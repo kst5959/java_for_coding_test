2511e17e35e7b1eaf5a3ca7e60d3cc4fc9530d43
kst5959 <52456639+kst5959@users.noreply.github.com>
2021-06-12 20:26:30 +0900

    N번 째 피보나치 수 구하기 (DP) 라이브러리

---
+++ CodingTest_Study/src/Library/fibonacci.java
@@ -0,0 +1,27 @@
+package Library;
+
+import java.util.Scanner;
+
+public class fibonacci {
+
+	public static int N;
+	public static int[] dp;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt();
+		dp = new int[N+1];
+		dp[0] = 0;
+		dp[1] = 1;
+		
+		for (int i = 2; i <= N; i++) 
+		{
+			dp[i] = dp[i-1] + dp[i-2];
+		}
+
+		System.out.println(dp[N]);
+	}
+
+}
